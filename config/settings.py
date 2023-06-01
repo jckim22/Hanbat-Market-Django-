e6fa013c1dde87da45470a49187f2a58051f9130
김주찬 <jckim2@gimjuchan-ui-MacBookAir.local>
2023-06-01 15:29:33 +0900

    1차 배포-3

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.kakao',
-    '(jckim2).pythonanywhere.com',
+    'jckim2.pythonanywhere.com',
 ]
 
 MIDDLEWARE = [
